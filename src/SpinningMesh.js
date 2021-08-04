c9b4c190031ffed6a378bb22050a2774209f7074
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-04 10:56:02 +0300

    Branched out mesh and canvast to own files

---
+++ src/SpinningMesh.js
@@ -0,0 +1,28 @@
+import React, { useRef, useState} from "react";
+import { useFrame } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
+import { useSpring, a } from "@react-spring/three";
+
+const SpinningMesh = ({position, args}) => {
+    const [expand, setExpand] = useState(false);
+    
+
+    const mesh = useRef(null);
+    useFrame( () => {mesh.current.rotation.x = mesh.current.rotation.y += 0.007});
+  
+    const [apandahTexture] = useTexture(["./Apandah.jpg"]);
+    
+  
+    const props = useSpring({
+      scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+    });
+  
+    return(
+        <a.mesh onClick={() => setExpand(!expand)} scale={props.scale} castShadow position={position} ref={mesh}>
+          <boxBufferGeometry attach="geometry" args={args} />
+          <meshStandardMaterial attach="material" map={apandahTexture} factor={0.3} />
+        </a.mesh>
+    );
+}
+
+export default SpinningMesh;
