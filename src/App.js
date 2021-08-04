7a09f29a0cd5c6f4071491d09b29595b2ca35e4b
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-04 11:06:29 +0300

    More Cleaning up, created header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React, { useRef, useState, Suspense } from 'react';
 import './App.scss';
 
 import MainCanvas from './Canvas';
-
+import Header from './Header';
 
 
 function App() {
   
 
   return (
-    <MainCanvas />
+    <>
+      <Header /> 
+
+      <MainCanvas />
+    
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./Header.module.scss";
+
+const Header = () => {
+    return(
+        <header className="header">
+            <h3>Pandah World</h3>
+            <h3>By Ciel</h3>
+        </header>
+    );
+}
+
+export default Header;
