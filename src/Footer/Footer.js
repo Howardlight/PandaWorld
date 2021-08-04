b11b6d853006a357eb0c17a05e26684f7fa70af9
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-04 22:33:16 +0300

    Switched Show and Switch Strings

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = ({toggleVisibility, isVisible, toggleShape, shape}) => {
 
     return(
         <footer className={classes.Footer}>
-            <h4 onClick={toggleVisibility} style={{marginRight: "auto"}}>{isVisible ? "Show" : "Hide"}</h4>
+            <h4 onClick={toggleVisibility} style={{marginRight: "auto"}}>{isVisible ? "Hide" : "Show"}</h4>
             <h4 onClick={toggleShape}>{shape ? "Change to Cube" : "Change to Circle"}</h4>
             {/* <h4>Button</h4> */}
         </footer>
