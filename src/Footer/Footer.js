83caec37d1f5bf37d6778120792495976aa46579
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-08 22:13:30 +0300

    Removed Console Log

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -14,7 +14,6 @@ const Footer = ({toggleShape, shape}) => {
     const handleIsHidden = () => {
         if(isHidden === false) dispatch(hide());
         else dispatch(show());
-        console.log(isHidden); // remove this later
     }
 
 
