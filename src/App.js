eaa4e4e14fa7251d0eb838e58b9af4ce51bcb721
Ciel/howard <howardlight+github@protonmail.com>
2021-09-21 23:28:03 +0300

    Added Background Music

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+// REACT RELATED
 import React, { useState } from 'react';
 import './App.scss';
 
+// MAIN SCRIPTS
 import MainCanvas from './MainCanvas/MainCanvas';
 import Header from './Header/Header';
 // import Footer from "./Footer/Footer";
 import SlideBar from "./Menu/SlideBar";
 
+// AUDIO AND MUSIC RELATED
+import useSound from 'use-sound';
+import BackgroundMusic from "./Assets/audio/bm.mp3";
 
 function App() {
   
+  // OPENING/CLOSING MENU STATE AND TOGGLE
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   }
 
-  //TODO: Add background music
+  // BACKGROUND MUSIC
+  // TODO: If possible port this into its own JS file
+  // TODO: Trim the Background music, mainly the beggining and the end
+  // USE AUDACITY
+  // Docs for useSound: https://www.joshwcomeau.com/react/announcing-use-sound-react-hook/
+  // TODO: Add a mute button
+  const [playBackMusic] = useSound(
+    BackgroundMusic,
+    { volume: 0.01 }
+  );
 
   //TODO: add sound effects,
   // Places where Sound effects can be added: 
@@ -27,16 +42,14 @@ function App() {
     <>
       <Header toggleMenu={toggleMenu}/> 
 
+      {playBackMusic()}
+
       <SlideBar
-      
-      
       isMenuOpen={isMenuOpen} 
-      toggleMenu={toggleMenu} 
-      
+      toggleMenu={toggleMenu}       
       />
 
       {/* <Footer /> */}
-
       <MainCanvas />
 
     </>
