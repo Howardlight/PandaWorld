1fa7b0995797e07736dafb9d8f9cf7748c6241d8
Ciel/howard <howardlight+github@protonmail.com>
2021-11-27 22:01:07 +0200

    Added shape Button with logic to Footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -22,8 +22,8 @@ const Footer = () => {
 
     
     // BUTTON TEXTS(CONTENTS) STATES
-    const [textureStateButtonName, setTextureStateButtonName] = useState("Aztrosist");
-
+    const [textureStateButtonName, setTextureStateButtonName] = useState("Apandah");
+    const [shapeStateButtonName, setShapeStateButtonName] = useState("Box");
 
     // handles visibility Boolean Logic for Zustand
     const handleIsHidden = () => {
@@ -31,29 +31,51 @@ const Footer = () => {
         else show();
     }
 
-    const handleSelectedTexture = () => {
+    const handleTextureState = () => {
         // ONCLICK will increment Texture state by 1
         // if reached last possible choice, restart to 0
-        if(texture === textureArr.length -1 ){
+        if(texture === textureArr.length - 1 ){
             setToAmountTexture(0);
-        }
-        else setToAmountTexture(texture+1);
+        } else setToAmountTexture(texture+1);
 
         // Changes name of button to reflect CURRENT texture
         setTextureStateButtonName(textureArr[handleTextureStateButtonName(texture)].name);
     }
 
 
-    //TODO: maybe change this name lol
     const handleTextureStateButtonName = (texture) => {
-        // TODO: Comment and explain this
+        //TODO: Maybe abstract this func and the shape one to only one function, they are very
+        // similar
 
 
+        // WHAT: if texture state reaches last correct value, return to 0,
+        // WHY: this is to give the array a valid index
+        //  otherwise it would cause an error, as clicking the button til the loop
+        //  will give -1, and 4, which are invalid array indexes
         if(texture === 3) return 0;
         return texture+1;
     }
 
-    // TODO: Add more Buttons that control Texture
+    const handleShapeState = () => {
+        // ONCLICK will increment shape state by 1
+        // if reached last possible choice, restart to 0
+        if(shape === Shapes.length - 1){
+            setToShape(0);
+        } else setToShape(shape+1);
+
+        // changes name of button to reflect CURRENT shape;
+        setShapeStateButtonName(Shapes[handleShapeStateButtonName(shape)]);
+    }
+
+    const handleShapeStateButtonName = (shape) => {
+        // WHAT: if texture state reaches last correct value, return to 0,
+        // WHY: this is to give the array a valid index
+        //  otherwise it would cause an error, as clicking the button til the loop
+        //  will give -1, and 4, which are invalid array indexes
+        if(shape === 7) return 0;
+        else return shape+1;
+    }
+
     // TODO: Add night time button, adds Stars, removes sun, darkens light emitters
     // TIP: Use Leva or react-three/gui
     // All components that will act as a UI MUST be overlapped over the canvas
@@ -61,7 +83,8 @@ const Footer = () => {
     return(
         <footer className={classes.Footer}>
             <button onClick={handleIsHidden}>{isHidden ? "Show" : "Hide"}</button>
-            <button onClick={handleSelectedTexture}>{textureStateButtonName}</button>
+            <button onClick={handleTextureState}>{textureStateButtonName}</button>
+            <button onClick={handleShapeState}>{shapeStateButtonName}</button>
         </footer>
     );
 }
