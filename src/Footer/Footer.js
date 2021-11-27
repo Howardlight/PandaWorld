d9d1542f2f8a36e8d5afc5ae9cc07935c654d8b7
Ciel/howard <howardlight+github@protonmail.com>
2021-11-27 21:26:06 +0200

    Added TextureChange logic to Footer Buttons

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,14 +1,29 @@
-import React from "react";
+import {React, useState} from "react";
 import { useStore } from "../redux/store/ZustandStore";
 import classes from "./Footer.module.scss";
 
+import { Shapes, textureArr } from "../MainCanvas/ObjectMesh/Utils";
 
 const Footer = () => {
 
-    // related to Visibility of Mesh
+    // MESH VISIBILITY STORES
     const isHidden = useStore(state => state.isHidden);
     const hide = useStore(state => state.hideIsHidden);
     const show = useStore(state => state.showIsHidden);
+
+    // CURRENT TEXTURE STORES
+    const texture = useStore(state => state.texture);
+    const setToAmountTexture = useStore(state => state.setToAmountTexture);    
+
+    // CURRENT SHAPE STORE
+    const shape = useStore(state => state.shape)
+    const setToShape = useStore(state => state.setToShape)
+
+
+    
+    // BUTTON TEXTS(CONTENTS) STATES
+    const [textureStateButtonName, setTextureStateButtonName] = useState("Aztrosist");
+
 
     // handles visibility Boolean Logic for Zustand
     const handleIsHidden = () => {
@@ -16,6 +31,27 @@ const Footer = () => {
         else show();
     }
 
+    const handleSelectedTexture = () => {
+        // ONCLICK will increment Texture state by 1
+        // if reached last possible choice, restart to 0
+        if(texture === textureArr.length -1 ){
+            setToAmountTexture(0);
+        }
+        else setToAmountTexture(texture+1);
+
+        // Changes name of button to reflect CURRENT texture
+        setTextureStateButtonName(textureArr[handleTextureStateButtonName(texture)].name);
+    }
+
+
+    //TODO: maybe change this name lol
+    const handleTextureStateButtonName = (texture) => {
+        // TODO: Comment and explain this
+
+
+        if(texture === 3) return 0;
+        return texture+1;
+    }
 
     // TODO: Add more Buttons that control Texture
     // TODO: Add night time button, adds Stars, removes sun, darkens light emitters
@@ -24,7 +60,8 @@ const Footer = () => {
 
     return(
         <footer className={classes.Footer}>
-            {/* <button onClick={handleIsHidden}>{isHidden ? "Show" : "Hide"}</button> */}
+            <button onClick={handleIsHidden}>{isHidden ? "Show" : "Hide"}</button>
+            <button onClick={handleSelectedTexture}>{textureStateButtonName}</button>
         </footer>
     );
 }
