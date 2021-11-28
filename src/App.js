40f4cf399f82aecc11666af244d5070c7d493bf7
Ciel/howard <howardlight+github@protonmail.com>
2021-11-28 19:24:46 +0200

    Removed SlideBar Code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 // REACT RELATED
-import {
-  React,
-  } from 'react';
+import { React } from 'react';
 import './App.scss';
 
 // MAIN SCRIPTS
 import MainCanvas from './MainCanvas/MainCanvas';
 import Header from './Header/Header';
 import Footer from "./Footer/Footer";
-// import SlideBar from "./Menu/SlideBar";
-
 
 function App() {
-  
-  // PHASED OUT //
-  
-  // OPENING/CLOSING MENU STATE AND TOGGLE
-  // const [isMenuOpen, setIsMenuOpen] = useState(false);
-  // const toggleMenu = () => {
-  //   setIsMenuOpen(!isMenuOpen);
-  // }
-
-  // TODO: remove slidebar Code when ready
-  // REMOVE WHEN READY
-  // PHASED OUT //
-
-  //TODO: REDO THE USER INTERFACE
   return (
     <>
-      <Header 
-      // toggleMenu={toggleMenu}
-      />
-
-      {/* <SlideBar
-      isMenuOpen={isMenuOpen} 
-      toggleMenu={toggleMenu}       
-      /> */}
-
+      <Header />
       <Footer />
-      <MainCanvas />
 
+      <MainCanvas />
     </>
   );
 }
